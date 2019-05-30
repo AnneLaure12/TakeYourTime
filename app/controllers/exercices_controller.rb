455786938a8cf25a2188b1f7c80f1563643eba51
moral nicolas <mrwallp@gmail.com>
2019-05-30 14:48:28 +0200

    ExoFilterList

--- app/controllers/exercices_controller.rb
+++ app/controllers/exercices_controller.rb
@@ -1,11 +1,17 @@
 class ExercicesController < ApplicationController
   def index
-    @exercices = Exercice.all
+    search = params["search"]
+    if search.present?
+      name = search["name"]
+      @exercices = Exercice.ransack(name_or_description_cont: name).result
+    else
+      @exercices = Exercice.all
+    end
   end
 
   def show
     @exercices = Exercice.all
-    @exercice = Exercice.find(params[:id])
+    @exercice = Exercice.find(params[:mood_id])
     @achievement = Achievement.new
     @mood = @exercice.mood
   end
