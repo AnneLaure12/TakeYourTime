9a2c382227d289cd6fc56c6d8ef87a8f60b5f23a
Anne Laure Voisin <annelaurevoisin@gmail.com>
2019-05-31 14:12:38 +0200

    homepage done plus dashboard under construction

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,58 @@
 import "bootstrap";
+var Highcharts = require('highcharts');
+
+// Load module after Highcharts is loaded
+require('highcharts/modules/exporting')(Highcharts);
+
+// Create the chart
+Highcharts.chart('container', {
+    chart: {
+        type: 'spline'
+    },
+    title: {
+        text: 'Follow your achievement!'
+    },
+    xAxis: {
+        title: {
+          text: 'Month'
+        },
+        categories: ['', '', '', '', '', '',
+            '', '', '', '', '', '']
+    },
+    yAxis: {
+        title: {
+            text: 'Improvement'
+        },
+        labels: {
+            formatter: function () {
+                return this.value + '°';
+            }
+        }
+    },
+    tooltip: {
+        crosshairs: true,
+        shared: true
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                radius: 4,
+                lineColor: '#666666',
+                lineWidth: 1
+            }
+        }
+    },
+    series: [{
+        name: 'Tokyo',
+        marker: {
+            symbol: 'square'
+        },
+        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
+            y: 26.5,
+            marker: {
+                symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
+            }
+        }, 23.3, 18.3, 13.9, 9.6]
+
+    }]
+});
