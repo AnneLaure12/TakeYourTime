9244da3aa77762dfec6b7ab1d18274667672f44d
moral nicolas <mrwallp@gmail.com>
2019-06-03 11:35:26 +0200

    bookingController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+
   def index
     @bookings = Booking.where(user: current_user)
   end
@@ -9,6 +10,14 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @classes = Classe.all
+    @classe = Classe.find(params[:classe_id])
+    @booking = Booking.new(set_booking_params)
+    @booking.classe = @classe
+    @booking.user = current_user
+    if @booking.save
+      redirect_to @classes_path
+    else render 'classes/index'
   end
 
   def edit
@@ -18,5 +27,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_booking_params
+    params.require(:booking).permit(:title)
   end
 end
