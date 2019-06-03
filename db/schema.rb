0a2542ee738197e940a51b62d9360e766f904b9b
moral nicolas <mrwallp@gmail.com>
2019-06-03 18:27:53 +0200

    geocoderGem + migrationCoordinatesToTeachers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_152207) do
+ActiveRecord::Schema.define(version: 2019_06_03_162621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2019_06_03_152207) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
