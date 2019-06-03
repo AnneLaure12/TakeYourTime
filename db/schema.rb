af781aee92c3f5d197e8f0ac7b2969efd645229f
moral nicolas <mrwallp@gmail.com>
2019-06-03 17:23:51 +0200

    addDateToBookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_123414) do
+ActiveRecord::Schema.define(version: 2019_06_03_152207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_123414) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.bigint "teacher_id"
+    t.date "date"
     t.index ["teacher_id"], name: "index_bookings_on_teacher_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -61,7 +62,6 @@ ActiveRecord::Schema.define(version: 2019_06_03_123414) do
     t.string "name"
     t.string "address"
     t.text "description"
-    t.date "start_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
