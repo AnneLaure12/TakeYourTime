66786f3dc87b11cd02670c83df142360874a9c74
moral nicolas <mrwallp@gmail.com>
2019-06-03 11:15:11 +0200

    addTitleToBookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def new
+    @classe = Classe.find(params[:classe_id])
+    @booking = Booking.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
